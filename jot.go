4795b111fa776bebd82fdba4065012bf580a1814
Will Greenberg <will@endlessm.com>
2014-10-08 03:54:04 -0700

    Added CLI interface for grep and list

--- jot.go
+++ jot.go
@@ -8,29 +8,95 @@ import "path/filepath"
 import "github.com/wgreenberg/jot/io"
 import "github.com/wgreenberg/jot/config"
 
+type Action int
+const (
+    ACTION_UNKNOWN = iota
+    ACTION_OPEN Action = iota
+    ACTION_LOCK Action = iota
+    ACTION_UNLOCK Action = iota
+    ACTION_GREP Action = iota
+    ACTION_LIST Action = iota
+)
+
 func ensureJotDirExists() {
     if _, err := os.Stat(config.GetJotDir()); os.IsNotExist(err) {
         os.MkdirAll(config.GetJotDir(), 0777)
     }
 }
 
+func GetJotAction() Action {
+    var command string
+    if len(os.Args) == 1 {
+        command = "open"
+    } else {
+        command = os.Args[1]
+    }
+
+    if (command == "open") {
+        return ACTION_OPEN
+    } else if (command == "grep") {
+        return ACTION_GREP
+    } else if (command == "lock") {
+        return ACTION_LOCK
+    } else if (command == "unlock") {
+        return ACTION_UNLOCK
+    } else if (command == "ls") {
+        return ACTION_LIST
+    }
+    return ACTION_OPEN
+}
+
 // Opens the editor to a new jot
-func CreateNewJot() {
-    newJotName := io.UniqueName()
-    newJotPath := filepath.Join(config.GetJotDir(), newJotName)
+func CreateNewJot(name string) {
+    if name == "" {
+        name = io.UniqueName()
+    }
+    newJotPath := filepath.Join(config.GetJotDir(), name)
     cmd := exec.Command(config.GetJotEditor(), newJotPath)
     cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
     err := cmd.Run()
     if err != nil {
-        fmt.Println("Error creating the jot:", err)
+        fmt.Printf("Error creating the jot: %s\n", err)
+    } else {
+        fmt.Printf("Successfully made jot %s\n", name)
     }
 }
 
 func main() {
     // setup our jot dir if it doesn't exist
     ensureJotDirExists()
+    action := GetJotAction()
 
-    CreateNewJot()
+    switch {
+    case ACTION_OPEN == action:
+        if len(os.Args) == 1 {
+            CreateNewJot("")
+        } else {
+            CreateNewJot(os.Args[1])
+        }
+    case ACTION_LOCK == action:
+    case ACTION_UNLOCK == action:
+    case ACTION_GREP == action:
+        jots := io.ReadAllJots()
+        if len(os.Args) == 2 {
+            fmt.Printf("Error, grep needs a pattern to search against\n")
+            return
+        }
+        for _, jot := range jots {
+            if len(jot.Find(os.Args[2])) > 0 {
+                for _, line := range jot.Find(os.Args[2]) {
+                    fmt.Printf("%s: %s\n", jot.Name(), line)
+                }
+            }
+        }
+    case ACTION_LIST == action:
+        jots := io.ReadAllJots()
+        for _, jot := range jots {
+            fmt.Printf("%s: %s\n", jot.Name(), jot.Title())
+        }
+    case ACTION_UNKNOWN == action:
+        fmt.Printf("Error, don't know command: %s\n", os.Args)
+    }
 }
